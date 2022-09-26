4f7801769da214a59ce9da7c1a1881006f05129a
Ken <fieldken@hotmail.com>
2022-09-26 12:35:07 +0200

    Add project files.

---
+++ EFCoreVoorbeelden/Migrations/20220926103103_Courses.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EFCoreVoorbeelden.Migrations
+{
+    public partial class Courses : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Students",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Age",
+                table: "Students",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.CreateTable(
+                name: "Grades",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Year = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Grades", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Grades");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Students",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(50)",
+                oldMaxLength: 50);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Age",
+                table: "Students",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+        }
+    }
+}
---
+++ EFCoreVoorbeelden/Program.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+Console.WriteLine("Hello, World!");
+
+using (ApplicationDbContext ctx = new ApplicationDbContext())
+{
+    //TOEVOEGEN
+    /*ctx.Students.Add(new Student("Ken", 26, "Stationstraat 1"));
+    ctx.Students.Add(new Student()
+    {
+        Name = "Arno",
+        Age = 28,
+        Address = "Stationstraat 2"
+    });
+
+    ctx.SaveChanges();*/
+
+    //LEZEN
+    foreach (var s in ctx.Students.OrderByDescending(s => s.Age))
+    {
+        Console.WriteLine($"{s.Name} - {s.Age} - {s.Address}");
+    }
+
+    int idUitCombobox = 1;
+    Student? student = ctx.Students.FirstOrDefault(s => s.Id == idUitCombobox);
+                                                   //IK WIL EEN STUDENT SELECTEREN
+                                                     //WAARVAN
+                                                        //DE STUDENT ZIJN ID
+                                                             //HETZELFDE IS DAN
+                                                                //HET ID UIT DE COMBOBOX
+
+    Console.WriteLine(student.Name);
+
+    //UPDATE
+    int idVanArno = 2;
+    Student st = ctx.Students.FirstOrDefault(s => s.Id == idVanArno);
+    st.Name = "Emlyn";
+    ctx.SaveChanges();
+
+    //REMOVE
+    /*int idVanMathias = 3;
+    Student st = ctx.Students.FirstOrDefault(s => s.Id == idVanMathias);
+    ctx.Students.Remove(st);
+    ctx.SaveChanges();*/
+
+}
+
+public class ApplicationDbContext : DbContext
+{
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer(@"Server=(localdb)\MSSQLLocalDB;Database=ApplicationDatabase;Trusted_Connection=True;"); 
+    }
+
+    public DbSet<Student> Students { get; set; }
+    public DbSet<Grade> Grades { get; set; }
+}
+
+
+public class Student
+{
+    public int Id { get; set; }
+    [MaxLength(50)] public string Name { get; set; }
+    public int? Age { get; set; }
+    public string Address { get; set; }
+
+    public Student()
+    {
+
+    }
+
+    public Student(string name, int age, string address)
+    {
+        Name = name;
+        Age = age;
+        Address = address;
+    }
+}
+
+public class Grade
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public int Year { get; set; }
+}
